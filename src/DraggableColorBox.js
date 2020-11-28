dbae838d627263a6ee538f21456ebce299aa4919
OlaFro <olka.be@gmail.com>
2020-11-28 11:28:58 +0100

    buttons added and styled

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,33 +1,27 @@
 import React from "react";
-import IconButton from "@material-ui/core/IconButton";
-import DeleteIcon from "@material-ui/icons/Delete";
 import styles from "./styles/DraggableColorBoxStyles";
 import { withStyles } from "@material-ui/core/styles";
 import { SortableElement } from "react-sortable-hoc";
 
 const DraggableColorBox = SortableElement((props) => {
-  const { classes, deleteBox, id, color, palette, setPalette } = props;
+  const { classes, id, color, deleteBox } = props;
+
   return (
     <div className={classes.box} style={{ backgroundColor: color }}>
-      <div>
-        <span className={classes.text}>{`${color}`}</span>
-
-        <IconButton
-          aria-label="delete"
-          className={classes.icon}
-          style={{ color: "white" }}
-        >
-          {console.log(id)}
-          <button
-            onClick={() => {
-              console.log("test");
-              let filtered = palette.filter((elem) => elem.id !== id);
-              setPalette(filtered);
-            }}
-          >
-            <DeleteIcon />
-          </button>
-        </IconButton>
+      <span className={classes.colorNumber}>{`${color}`}</span>
+      <div className={classes.buttons}>
+        <a href="#" className={classes.button}>
+          shades
+        </a>
+        <a href="#" className={classes.button}>
+          copy
+        </a>
+        <a href="#" className={classes.button}>
+          move
+        </a>
+        <button className={classes.button} onClick={() => deleteBox(id)}>
+          del
+        </button>
       </div>
     </div>
   );
--- src/styles/DraggableColorBoxStyles.js
+++ src/styles/DraggableColorBoxStyles.js
@@ -3,23 +3,62 @@ export default {
     flex: "1",
     height: "100%",
     display: "flex",
+    flexGrow: "1",
     flexDirection: "column",
     color: "white",
-    textTransform: "uppercase",
     fontFamily: "'Quicksand', monospace",
     justifyContent: "space-around",
     alignItems: "space-around",
     position: "relative",
+    transition: "200ms",
+    "&:hover": {
+      flexGrow: "2",
+    },
   },
-  text: {
+
+  colorNumber: {
     position: "absolute",
     bottom: "20px",
     left: "20px",
+    textTransform: "uppercase",
   },
-  icon: {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translateX(-50%)",
+
+  buttons: {
+    margin: "auto",
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    height: "60%",
+    alignItems: "center",
+    justifyContent: "space-around",
+    opacity: "0",
+    cursor: "pointer",
+    "&:hover": {
+      opacity: "1",
+    },
+  },
+
+  button: {
+    textDecoration: "none",
+    fontFamily: "'Quicksand', monospace",
+    fontSize: "1rem",
+    width: "4.7rem",
+    height: "2rem",
+    border: "none",
+    borderRadius: "15px",
+    backgroundColor: "transparent",
+    color: "rgba(255,255,255,0.5)",
+
+    "&:hover": {
+      backgroundColor: "rgba(255,255,255,0.1)",
+    },
+
+    "&:active": {
+      outline: "none",
+      backgroundColor: "rgba(255,255,255,0.5)",
+    },
+    "&:focus": {
+      outline: "none",
+    },
   },
 };
